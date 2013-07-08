ae2513c3a29e8fc5243e14c421b36c3d1c25748a
dev <dev@dev>
2013-07-08 18:59:35 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Knoda::Application.config.secret_key_base = '7739b3efe94874bb94ceff389effe0c0d5cfaefcb6d599163a5da314b7846cb8b2f153e73ce7fdecbafd9503f9202b0d6524130d86e991aeb17891548cae7781'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Knoda::Application.config.session_store :cookie_store, key: '_knoda_session'
