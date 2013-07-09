6e753fc4dc50f0e0ccabf042c7646496ef9e179a
dev <dev@dev>
2013-07-09 16:36:34 +0400

    added validator for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates_presence_of   :username
+  validates_uniqueness_of :username
+  #validates_length_of     :username, :minimum => 1, :maximum => 15
+  validates_format_of     :username, :with => /\A[a-zA-Z0-9_]{1,15}\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130708150741) do
+ActiveRecord::Schema.define(version: 20130709114122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 20130708150741) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
+    t.string   "username"
+    t.boolean  "notifications"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
