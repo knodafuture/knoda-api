1d59f80609b3541f20bedc2da69c8186583961ff
dev <dev@dev>
2013-07-09 17:49:27 +0400

    Removes unusable code in application controller and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ActiveRecord::Base
          
   validates_presence_of   :username
   validates_uniqueness_of :username
-  #validates_length_of     :username, :minimum => 1, :maximum => 15
   validates_format_of     :username, :with => /\A[a-zA-Z0-9_]{1,15}\z/
   
   attr_accessor :login
