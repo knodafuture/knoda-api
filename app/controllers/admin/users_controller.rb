530c907aa1b8a88e9ceb1988881519ec980b1b99
Alexander Nusov <alexander.nusov@dataart.com>
2013-07-10 15:28:57 +0400

    Adds basic features for admin/users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,43 @@
 class Admin::UsersController < Admin::AdminController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_url }
+      format.json { head :no_content }
+    end
+  end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
