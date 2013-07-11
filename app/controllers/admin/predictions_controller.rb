f9466dc7ec1f994f2a9f22a94f7806f1ecf0534c
Alexander Nusov <alexander.nusov@dataart.com>
2013-07-11 18:54:32 +0400

    Adds admin/prediction controller

--- app/controllers/admin/predictions_controller.rb
+++ app/controllers/admin/predictions_controller.rb
@@ -1,2 +1,22 @@
 class Admin::PredictionsController < Admin::AdminController
+  before_action :set_prediction, only: [:update]
+  
+  def index
+    list = params[:list] || 'expiring'
+    @predictions = case list
+      when 'expiring' then Prediction.order("expires_at DESC")
+      when 'new'      then Prediction.order("created_at DESC")
+      when 'top'      then Prediction.order("created_at DESC") # TODO: order by votes count
+    end
+  end
+  
+  def update
+    
+  end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_prediction
+      @prediction = Prediction.find(params[:id])
+    end
 end
