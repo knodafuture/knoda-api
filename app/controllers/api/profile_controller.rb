2ebb6fdbfa4841acde62d61feb0f0246b68709ce
Alexander Nusov <alexander.nusov@dataart.com>
2013-07-11 14:30:05 +0400

    Fixes ActionController::InvalidAuthenticityToken

--- app/controllers/api/profile_controller.rb
+++ app/controllers/api/profile_controller.rb
@@ -1,4 +1,5 @@
 class Api::ProfileController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_filter :require_login
   before_action :set_user, :only => [:show, :changepassword]
   
