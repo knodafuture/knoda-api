d2a9c183ab67f7f5fd74b59c70c742d74fae040b
Alexander Nusov <alexander.nusov@dataart.com>
2013-07-11 16:33:29 +0400

    Adds edit email feature in CMS

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,7 +14,7 @@ class Admin::UsersController < Admin::AdminController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to [:admin, @user], notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
