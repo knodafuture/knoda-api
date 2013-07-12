18cba67a168ae702bc1bc8ac5aa831853b305152
Alexander Nusov <alexander.nusov@dataart.com>
2013-07-12 20:48:30 +0400

    Fixes paddings in challenge model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,9 +1,7 @@
 class Challenge < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :prediction
 
-belongs_to :user
-belongs_to :prediction
-
-validates :user_id, presence: true
-validates :prediction_id, presence: true
-
+  validates :user_id, presence: true
+  validates :prediction_id, presence: true
 end
