8acfbf70431cd8be639020c87d6d63c5095f3e9f
Alexander Nusov <alexander.nusov@local>
2013-07-15 20:38:56 +0400

    Refactoring of api/password test

--- spec/controllers/api/passwords_controller_spec.rb
+++ spec/controllers/api/passwords_controller_spec.rb
@@ -4,18 +4,15 @@ describe Api::PasswordsController do
   describe "Change password using password.json" do
     it "should change password" do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      
-      # FactoryGirl.find_definitions
+
       @user = FactoryGirl.build :user
-      @user.password = 'password123'
-      @user.password_confirmation = 'password123'
       @user.reset_authentication_token!
       @user.save!
       
       patch :update, {
-        auth_token: @user.authentication_token, 
-        current_password: 'password123', 
-        new_password: 'password321'}, :format => :json
+        auth_token:       @user.authentication_token, 
+        current_password: @user.password_confirmation, 
+        new_password:     @user.password_confirmation.reverse}, :format => :json
       
       response.status.should eq(204);
     end
