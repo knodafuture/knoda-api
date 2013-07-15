7a3c2908ae37c1b27a05ca48811077604e9718ca
Alexander Nusov <alexander.nusov@local>
2013-07-15 19:56:07 +0400

    Permits updating notification parameter for user model

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -26,7 +26,7 @@ class Api::UsersController < ApplicationController
   end
   
   def user_params
-    params.require(:user).permit(:avatar)
+    params.require(:user).permit(:avatar, :notifications)
   end
   
   def require_login
