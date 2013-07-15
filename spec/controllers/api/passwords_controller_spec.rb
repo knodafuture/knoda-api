add4635c20579519919f5445f73650dbd8bec288
Alexander Nusov <alexander.nusov@local>
2013-07-15 19:33:51 +0400

    Adds API password test

--- spec/controllers/api/passwords_controller_spec.rb
+++ spec/controllers/api/passwords_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Api::PasswordsController do
-
+  describe "Change password using password.json" do
+    it "should change password" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['CONTENT_TYPE'] = 'application/json'
+      @request.env['ACCEPT'] = 'application/json'
+      
+      # FactoryGirl.find_definitions
+      @user = FactoryGirl.build :user
+      @user.password = 'password123'
+      @user.password_confirmation = 'password123'
+      @user.reset_authentication_token!
+      @user.save!
+      
+      patch :update, {
+        auth_token: @user.authentication_token, 
+        current_password: 'password123', 
+        new_password: 'password321'}, :format => :json
+      
+      response.status.should eq(204);
+    end
+  end
 end
