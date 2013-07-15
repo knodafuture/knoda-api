adbffe2dfc3b4b2ee46bd70bda02a94902c2bc66
Alexander Nusov <alexander.nusov@local>
2013-07-15 15:17:23 +0400

    Fixes pages controller test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -23,11 +23,4 @@ describe PagesController do
     end
   end
 
-  describe "GET 'blog'" do
-    it "returns http success" do
-      get 'blog'
-      response.should be_success
-    end
-  end
-
 end
