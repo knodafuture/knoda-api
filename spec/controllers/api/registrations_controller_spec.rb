cdea874024f9988e07720796e5d4af327124c906
Alexander Nusov <alexander.nusov@local>
2013-07-15 19:34:04 +0400

    Adds API registration tests

--- spec/controllers/api/registrations_controller_spec.rb
+++ spec/controllers/api/registrations_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Api::RegistrationsController do  
-  
+  describe "POST registration.json" do
+    it "should sign up using registration.json" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['CONTENT_TYPE'] = 'application/json'
+      @request.env['ACCEPT'] = 'application/json'
+      
+      FactoryGirl.find_definitions
+      @user = FactoryGirl.build :user
+      
+      Rails.logger.info "USER"
+      Rails.logger.info @user.username
+      Rails.logger.info @user.password
+      Rails.logger.info @user.email
+      
+      post :create, {:user => {
+        :username   => @user.username,
+        :email      => @user.email,
+        :password   => @user.password
+      }}, :format => :json
+      
+      response.status.should eq(200)
+      json = JSON.parse(response.body)
+      
+      json["success"].should eq(true)
+      
+      token = json["auth_token"]
+      
+      user = User.find_by(:authentication_token => token)
+      user.should_not be_nil
+      
+      user.email.should eq(@user.email)
+    end
+  end
 end
