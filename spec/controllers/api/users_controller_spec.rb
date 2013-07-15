1eeaa185720067492d07a8956c5f181bb2e1ee1f
Alexander Nusov <alexander.nusov@local>
2013-07-15 19:34:31 +0400

    Adds API users tests

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Api::UsersController do
-
+  describe "GET user.json" do
+    it "should return user information" do
+      @user = FactoryGirl.build :user
+      @user.password = 'password123'
+      @user.password_confirmation = 'password123'
+      @user.reset_authentication_token!
+      @user.save!
+      
+      get :show, auth_token: @user.authentication_token, :format => :json
+    
+      response.status.should eq(200)
+      json = JSON.parse(response.body)
+      json["username"].should eq(@user.username)
+      json["email"].should eq(@user.email)
+    end
+  end
+  
+  #describe "PUT user.json" do
+  #  it "should update avatar" do
+  #    @user = FactoryGirl.build :user
+  #    @user.password = 'password123'
+  #    @user.password_confirmation = 'password123'
+  #    @user.reset_authentication_token!
+  #    @user.save!
+  #    
+  #    put :update, auth_token: @user.authentication_token, :user => {}, :format => :json
+  #    
+  #    response.status.should eq(200)
+  #  end
+  #end
 end
