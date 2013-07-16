127aa162b33984c57e3c8800ef1e49fd257c22a1
Alexander Nusov <alexander.nusov@local>
2013-07-16 16:40:56 +0400

    Adds ChallengeAuthorizer

---
+++ app/authorizers/challenge_authorizer.rb
@@ -0,0 +1,17 @@
+class ChallengeAuthorizer < ApplicationAuthorizer
+  def self.creatable_by?(user)
+    true
+  end
+  
+  def self.readable_by?(user)
+    true
+  end
+  
+  def self.updatable_by?(user)
+    true
+  end
+  
+  def self.deletable_by?(user)
+    true
+  end  
+end
