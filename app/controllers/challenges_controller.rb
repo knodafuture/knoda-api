1f0ef77fb9748931df628f757dbc19d6ca6ce053
Alexander Nusov <alexander.nusov@local>
2013-07-17 14:52:57 +0400

    Removes require_login in Challenges controller

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,5 +1,4 @@
 class ChallengesController < ApplicationController
-  before_filter :require_login
   before_action :set_challenge, only: [:show]
   respond_to :html, :json
   
@@ -22,16 +21,5 @@ class ChallengesController < ApplicationController
 
   def prediction_params
     params.require(:prediction).permit(:user_id, :prediction_id, :is_positive)
-  end
-
-  def require_login
-    unless current_user
-      flash[:error] = "You must be logged in to access this section"
-
-      respond_to do |format|
-        format.html { redirect_to new_user_session_url }
-        format.json { render json: {success: false}, :status => 403 }
-      end
-    end
   end
 end
