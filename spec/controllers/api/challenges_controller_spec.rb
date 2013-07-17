c30f368402c6b7d69637c9cf72a94c0fbd916b9c
Alexander Pasechnik <Alexander.Pasechnik@dataart.com>
2013-07-17 19:52:46 +0300

    Adds test controller for API::ChallengeController

--- spec/controllers/api/challenges_controller_spec.rb
+++ spec/controllers/api/challenges_controller_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe Api::ChallengesController do
 
+  user = login_as_user(true)
+
+  let(:valid_session) { {:auth_token => user.authentication_token} }
+
+  describe "GET challenges.json" do
+    it "should be successful response" do
+      challenge = Challenge.create(:user_id => User.all.first.id, :prediction_id => Prediction.all.first.id, :agree => 1)
+      get :index, {:format => :json}, valid_session
+      response.status.should eq(200)
+      json = JSON.parse(response.body)
+      json.should include("challenges")
+    end
+  end
+
 end
