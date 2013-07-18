b1c2a68313b96b66206534061fef4cd32f80ebbf
Alexander Nusov <alexander.nusov@local>
2013-07-18 19:14:05 +0400

    Validates tag count for prediction

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -8,6 +8,7 @@ class Prediction < ActiveRecord::Base
   validates :body, presence: true
   validates :expires_at, presence: true
   validates :tag_list, presence: true
+  validate  :max_tag_count
 
   scope :recent, :order => "predictions.created_at DESC"
   scope :expiring, lambda { { :conditions => ["predictions.expires_at >= ?", Time.now], :order => "predictions.expires_at ASC" } }
@@ -16,4 +17,10 @@ class Prediction < ActiveRecord::Base
   # Adds `creatable_by?(user)`, etc
   include Authority::Abilities
   self.authorizer_name = 'PredictionAuthorizer'
+  
+  private
+  
+  def max_tag_count
+    errors[:tag_list] << "1 tag maximum" if tag_list.count > 1
+  end
 end
