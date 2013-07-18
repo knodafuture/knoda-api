6c4299bb074c0a1653f7783af7fd478cee830478
Alexander Nusov <alexander.nusov@local>
2013-07-18 15:29:04 +0400

    Adds closed_at to prediction serialzer

--- app/serializers/prediction_serializer.rb
+++ app/serializers/prediction_serializer.rb
@@ -1,6 +1,6 @@
 class PredictionSerializer < ActiveModel::Serializer
 
-  attributes :id, :user_id, :body, :outcome, :expires_at, :created_at
+  attributes :id, :user_id, :body, :outcome, :expires_at, :created_at, :closed_at
 
   has_many :tags
 
