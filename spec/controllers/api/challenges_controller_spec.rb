a2825f78f1ef00c3eac3cd25483e75c49650a3b5
Alexander Pasechnik <Alexander.Pasechnik@dataart.com>
2013-07-18 13:54:50 +0300

    Adds test for show action in Api::ChallengesController

--- spec/controllers/api/challenges_controller_spec.rb
+++ spec/controllers/api/challenges_controller_spec.rb
@@ -17,4 +17,18 @@ describe Api::ChallengesController do
     end
   end
 
+  describe "GET show" do
+    it "should be successful response" do
+      prediction  = FactoryGirl.create :prediction
+      challenge   = Challenge.create(:user_id => User.all.first.id, :prediction_id => prediction.id, :agree => 1)
+      get :show, {:id => prediction.to_param, :format => :json}, valid_session
+      response.status.should eq(200)
+      json = JSON.parse(response.body)
+      json.should include("id")
+      json.should include("agree")
+      json.should include("user")
+      json.should include("prediction")
+    end
+  end
+
 end
