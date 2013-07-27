1246eb3b3897f2f593d3a0c653c3e30419e4a302
Alexander Nusov <alex@local>
2013-07-27 13:49:57 +0400

    Refactoring of prediction model

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,26 +1,31 @@
 class Prediction < ActiveRecord::Base
   acts_as_taggable
   
+  include Authority::Abilities
+  self.authorizer_name = 'PredictionAuthorizer'
+  
   after_create :prediction_create_badges
+  after_create :create_own_challenge
 
   belongs_to :user
+  
   has_many :challenges, :dependent => :destroy
   has_many :voters, through: :challenges, class_name: "User", source: 'user'
 
   validates :body, presence: true
   validates :expires_at, presence: true
+  validate  :expires_at_is_not_past
+  
   validates :tag_list, presence: true
   validate  :max_tag_count
   validate  :tag_existence
   
   validates_length_of :body, :maximum => 300
+  validates_uniqueness_of :body
 
   scope :recent, lambda {{ :conditions => ["predictions.expires_at >= current_date"], :order => "predictions.created_at DESC" } }
   scope :expiring, lambda { { :conditions => ["predictions.expires_at >= current_date"], :order => "predictions.expires_at ASC" } }
 
-  # Adds `creatable_by?(user)`, etc
-  include Authority::Abilities
-  self.authorizer_name = 'PredictionAuthorizer'
 
   def self.recent_by_user_id(id)
     Prediction.where(user_id: id.to_s)
@@ -33,39 +38,25 @@ class Prediction < ActiveRecord::Base
               .where("closed_at IS NOT NULL")
               .order("created_at DESC")
   end
-  
-  def user_username
-    self.user.username
-  end
-  
-  def user_avatar_image
-    self.user.avatar_image
-  end
 
   def disagreed_count
     self.challenges.find_all_by_agree(false).count
   end
 
   def agreed_count
-    self.challenges.find_all_by_agree(true).count + 1
+    self.challenges.find_all_by_agree(true).count
   end
   
-  
-  def ratio
-    prediction_market.round
+  def market_size
+    self.challenges.count
   end
-
+  
   def prediction_market
-    total = self.challenges.count  + 1
-    positive = self.agreed_count
-    
-    (positive.to_f / total) * 100.0
+    self.agreed_count.fdiv(self.market_size).round(2)
   end
-  
-  
   
   def market_size_points
-    case self.challenges.count
+    case self.market_size
       when 0..5
         0
       when 6..20
@@ -97,17 +88,25 @@ class Prediction < ActiveRecord::Base
   end
 
   private
+  
+  def expires_at_is_not_past
+    errors[:expires_at] << "is past" if self.expires_at.past?
+  end
 
   def max_tag_count
-    errors[:tag_list] << "1 tag maximum" if tag_list.count > 1
+    errors[:tag_list] << "1 tag maximum" if self.tag_list.count > 1
   end
   
   def tag_existence
-    tag_list.each do |tag_name|
+    self.tag_list.each do |tag_name|
       if Topic.where(name: tag_name).first.nil?
         errors[:tag_list] << "invalid tag #{tag_name}"
       end
     end
+  end
+  
+  def create_own_challenge
+    self.challenges.create!(user: self.user, agree: true, is_own: true)
   end
   
   def prediction_create_badges
--- app/serializers/prediction_serializer.rb
+++ app/serializers/prediction_serializer.rb
@@ -1,9 +1,19 @@
 class PredictionSerializer < ActiveModel::Serializer
-
-  attributes :id, :user_id, :body, :outcome, :expires_at, :created_at, :closed_at, :ratio, :agreed_count, :disagreed_count
-  attributes :user_username, :user_avatar_image
+  attributes :id, :user_id, :body, :outcome, :expires_at, :created_at, :closed_at
+  attributes :agreed_count, :disagreed_count
+  attributes :market_size, :prediction_market
+  
+  attributes :username, :user_avatar
   
   has_many :tags
 
   self.root = false
+  
+  def username
+    object.user.username
+  end
+  
+  def user_avatar
+    object.user.avatar_image
+  end
 end
