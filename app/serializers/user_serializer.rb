4bde7bd8506160e228d6ef0853e309aa5ae31431
Alexander Nusov <alex@local>
2013-07-27 14:27:55 +0400

    Moves streak_string to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,20 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :created_at, :avatar_image, :notifications, :points, :won, :lost, :streak, :streak_s, :winning_percentage
+  attributes :id, :username, :email, :created_at, :avatar_image, :notifications, :points, :won, :lost, :streak, :winning_percentage
+  attributes :streak_string
 
   self.root = false
+  
+  def streak_string
+    if self.streak == 0
+      return ""
+    end
+    
+    if self.streak > 0
+      return "W#{self.streak}"
+    end
+    
+    if self.streak < 0
+      return "L#{self.streak}"
+    end
+  end
 end
