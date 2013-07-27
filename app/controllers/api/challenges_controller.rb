10b7e670523a151389a1cce9ef948f81d0d1182b
Alexander Nusov <alex@local>
2013-07-27 19:32:35 +0400

    Updates challenges controller

--- app/controllers/api/challenges_controller.rb
+++ app/controllers/api/challenges_controller.rb
@@ -1,20 +1,50 @@
 class Api::ChallengesController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_action :set_challenge, :only => [:show]
 
-  authorize_actions_for Challenge
+  #authorize_actions_for Challenge
   respond_to :json
 
-  def index    
-    if params[:notifications]
-      @challenges = Challenge.get_notifications_by_user_id(current_user.id)
-    else
-      @challenges = current_user.challenges.where(is_own: false)
-    end
-    
+  def own
+    @challenges = current_user.challenges.own
     respond_with(@challenges)
   end
-
+  
+  def picks
+    @challenges = current_user.challenges.picks
+    respond_with(@challenges)
+  end
+  
+  def completed
+    @challenges = current_user.challenges.completed
+    respond_with(@challenges)
+  end
+  
+  def set_seen
+    params[:ids].split(',').each do |id|
+      challenge = current_user.challenges.where(id: id).first
+      challenge.seen = true
+      challenge.save
+    end
+    
+    head :no_content
+  end
+  
   def show
-    respond_with(Challenge.find_by_prediction_id(params[:id]))
+    respond_with(@challenge)
+  end
+  
+  def expired
+    @challenges = Challenge.joins(:prediction)
+      .where(user: current_user)
+      .where("is_closed is false and expires_at <= ?", Time.now)
+      .order("expires_at DESC")
+    respond_with(@challenges)
+  end
+  
+  private
+  
+  def set_challenge
+    @challenge = Challenge.find(params[:id])
   end
 end
