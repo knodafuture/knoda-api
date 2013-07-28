3e742b9843b99e0e6d17d630932ecec28cba795c
Alexander Nusov <alex@local>
2013-07-28 11:59:20 +0400

    Adds points history to challenge serializer

--- app/serializers/challenge_serializer.rb
+++ app/serializers/challenge_serializer.rb
@@ -1,9 +1,20 @@
 class ChallengeSerializer < ActiveModel::Serializer
 
   attributes :id, :agree, :created_at, :is_own
+  attributes :points
   
   has_one :user
   has_one :prediction
+  
 
   self.root = false
+  
+  def points
+    {
+      base_points: object.base_points,
+      outcome_points: object.outcome_points,
+      market_size_points: object.market_size_points,
+      prediction_market_points: object.prediction_market_points
+    }
+  end
 end
