74d576cc264dcd26eda47effc0abd378e058e062
Alexander Nusov <alex@local>
2013-07-28 11:43:16 +0400

    Updates challenge authorizer

--- app/authorizers/challenge_authorizer.rb
+++ app/authorizers/challenge_authorizer.rb
@@ -1,17 +1,5 @@
 class ChallengeAuthorizer < ApplicationAuthorizer
-  def self.creatable_by?(user)
-    true
-  end
-  
   def self.readable_by?(user)
-    true
-  end
-  
-  def self.updatable_by?(user)
-    true
-  end
-  
-  def self.deletable_by?(user)
     true
   end
   
