761380592e02d0fa6c4f3ef1fd1efd30125329d1
Alexander Nusov <alex@local>
2013-07-29 18:16:41 +0400

    Adds my_points in prediction_details

--- app/serializers/prediction_feed_serializer.rb
+++ app/serializers/prediction_feed_serializer.rb
@@ -2,8 +2,15 @@ class PredictionFeedSerializer < PredictionSerializer
   self.root = false
   
   attributes :my_challenge
+  attributes :my_points
   
   def my_challenge
-    current_user.challenges.where(prediction_id: object.id)
+    current_user.challenges.where(prediction_id: object.id).first
+  end
+  
+  def my_points
+    if self.my_challenge
+      self.my_challenge.points_details
+    end
   end
 end
