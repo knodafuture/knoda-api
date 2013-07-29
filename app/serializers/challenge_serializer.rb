f746ac64f18bebc9c81bbcef4f8d418fcf64aa58
Alexander Nusov <alex@local>
2013-07-29 17:59:57 +0400

    Adds BS to challenge serializr

--- app/serializers/challenge_serializer.rb
+++ app/serializers/challenge_serializer.rb
@@ -2,6 +2,7 @@ class ChallengeSerializer < ActiveModel::Serializer
 
   attributes :id, :agree, :created_at, :is_own
   attributes :points_details
+  attributes :bs
   
   has_one :user
   has_one :prediction
