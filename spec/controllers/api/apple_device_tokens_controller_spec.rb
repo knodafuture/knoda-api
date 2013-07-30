c24aecac990ec4ea373db641777835a1e4090fd7
Alexander Nusov <alex@local>
2013-07-30 17:00:20 +0400

    Updates apple_device_tokens test

--- spec/controllers/api/apple_device_tokens_controller_spec.rb
+++ spec/controllers/api/apple_device_tokens_controller_spec.rb
@@ -7,13 +7,34 @@ describe Api::AppleDeviceTokensController do
   describe "GET apple_device_tokens.json" do
     it "should be successful response" do
       get :index, {format: :json}, valid_session
+      json = JSON.parse(response.body)
+      json.should include("apple_device_tokens")
+      response.status.should eq(200)
+    end
+  end
+  
+  describe "GET apple_device_token/:id.json" do
+    it "should be successful response" do
+      token = FactoryGirl.create(:apple_device_token)
+      token.user = user
+      token.save
+      
+      get :show, {format: :json, id: token.id}, valid_session
+      json = JSON.parse(response.body)
+      json.should include("id")
+      json.should include("token")
+      json.should include("created_at")
       response.status.should eq(200)
     end
   end
 
   describe "POST apple_device_tokens.json" do
     it "should be successful response" do
-      post :create, {format: :json, auth_token: user.authentication_token, apple_device_token: {token: "TOKEN"}}
+      token = FactoryGirl.build(:apple_device_token)
+      expect {
+        post :create, {format: :json, auth_token: user.authentication_token, apple_device_token: {token: token.token}}
+      }.to change(AppleDeviceToken, :count).by(1)
+      
       response.status.should eq(201)
     end
   end
