d6ea854b0cea1ec663f98efc5346a2bc2cfffe8d
Alexander Nusov <alex@local>
2013-07-30 17:43:10 +0400

    Add tests for api/topics controller

--- spec/controllers/api/topics_controller_spec.rb
+++ spec/controllers/api/topics_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Api::TopicsController do
-
+  user = login_as_user(true)
+  let(:valid_session) { {:auth_token => user.authentication_token} }
+  
+  describe "GET index.json" do
+    it "should return list of topics" do
+      FactoryGirl.create(:topic)
+      
+      get :index, {format: :json}, valid_session
+      response.status.should eq(200)
+      json = JSON.parse(response.body)
+      json.should include("topics")
+    end    
+  end
+  
 end
