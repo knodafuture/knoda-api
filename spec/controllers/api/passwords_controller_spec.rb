b81b9a2b392260fb10892e5c3a16288c62dbd806
Alexander Nusov <alex@local>
2013-07-30 17:13:47 +0400

    Updates Passwords test

--- spec/controllers/api/passwords_controller_spec.rb
+++ spec/controllers/api/passwords_controller_spec.rb
@@ -1,18 +1,32 @@
 require 'spec_helper'
 
 describe Api::PasswordsController do
-  describe "Change password using password.json" do
+  let(:user) { FactoryGirl.build(:user) }
+  
+  describe "Forgot password" do
+    it "should send forgot password email for existing user" do
+      user.save
+      
+      post :create, {login: user.email, format: :json}
+      response.status.should eq(204)
+    end
+    
+    it "should return 404 for bad email" do
+      post :create, {login: "bad_email@example.com", format: :json}
+      response.status.should eq(404)
+    end
+  end
+  
+  
+  describe "Change password using password.json" do    
     it "should change password" do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-
-      @user = FactoryGirl.build :user
-      @user.reset_authentication_token!
-      @user.save!
+      user.reset_authentication_token!
+      user.save
       
       patch :update, {
-        auth_token:       @user.authentication_token, 
-        current_password: @user.password_confirmation, 
-        new_password:     @user.password_confirmation.reverse}, :format => :json
+        auth_token:       user.authentication_token, 
+        current_password: user.password_confirmation, 
+        new_password:     user.password_confirmation.reverse}, :format => :json
       
       response.status.should eq(204);
     end
