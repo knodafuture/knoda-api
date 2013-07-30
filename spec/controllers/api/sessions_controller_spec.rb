612d2d7cd4193654193a4c4eb3f38529255ec51d
Alexander Nusov <alex@local>
2013-07-30 18:17:26 +0400

    Updates sessions controller

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -1,28 +1,34 @@
 require 'spec_helper'
 
 describe Api::SessionsController do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:valid_attributes) { {login: user.username, password: user.password_confirmation}}
+  let(:invalid_attributes) { {login: "invalid"} }
+  
   describe "POST session.json" do
     
     it "should login using session.json" do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-          
-      @user = FactoryGirl.create :user
       
-      post :create, {:user => {
-        :login      => @user.username,
-        :password   => @user.password_confirmation
-      }}, :format => :json
+      post :create, {user: valid_attributes}, format: :json
 
       response.status.should eq(200)
       json = JSON.parse(response.body)
-      json["email"].should eq(@user.email);
+      json["email"].should eq(user.email);
       
       @token = json["auth_token"]
       
       user = User.find_by(:authentication_token => @token)
       user.should_not be_nil
       
-      user.email.should eq(@user.email)
+      user.email.should eq(user.email)
+    end
+    
+    it "should not login using invalid crendentials" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      
+      post :create, {user: invalid_attributes}, format: :json
+      response.status.should eq(403)      
     end
   end
 
@@ -30,12 +36,11 @@ describe Api::SessionsController do
   describe "DELETE session.json" do
     it "should destroy session" do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-
-      @user = FactoryGirl.create :user
-      @user.reset_authentication_token!
-      @user.save!
       
-      @token = @user.authentication_token
+      user.reset_authentication_token!
+      user.save!
+      
+      @token = user.authentication_token
       
       delete :destroy, {auth_token: @token}, :format => :json
       user = User.find_by(:authentication_token => @token)
