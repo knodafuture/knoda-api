f03df61faed60e09da7eb19be3a2cce2b43c571b
Alexander Nusov <alex@local>
2013-07-31 12:44:03 +0400

    No user confirmation

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -6,18 +6,12 @@ class Api::RegistrationsController < Devise::RegistrationsController
     build_resource(sign_up_params)
  
     if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_up(resource_name, resource)
-        sign_in(resource_name, resource)
-        resource.reset_authentication_token!
-        resource.save!
-        return render :json => {:success => true, :auth_token => resource.authentication_token}
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        return render :json => {:success => true}
-      end
+      set_flash_message :notice, :signed_up if is_navigational_format?
+      sign_up(resource_name, resource)
+      sign_in(resource_name, resource)
+      resource.reset_authentication_token!
+      resource.save!
+      return render :json => {:success => true, :auth_token => resource.authentication_token}
     else
       clean_up_passwords resource
       return render :json => {:success => false, :errors => resource.errors}, :status => 400
