88413bb9d71a2e43105b71d3739b992d253cef68
Alexander Nusov <alex@local>
2013-07-31 12:10:10 +0400

    Removes deletable_by from prediction authorizer

--- app/authorizers/prediction_authorizer.rb
+++ app/authorizers/prediction_authorizer.rb
@@ -10,11 +10,7 @@ class PredictionAuthorizer < ApplicationAuthorizer
   def updatable_by?(user)
     user.id == resource.user_id
   end
-  
-  def deletable_by?(user)
-    user.id == resource.user_id
-  end
-  
+    
   def agreeable_by?(user)
     user.id != resource.user_id
   end
