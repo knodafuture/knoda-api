ffbe76686be9ca98f425b28eedaa32adb38a8ae1
Alexander Nusov <alex@local>
2013-08-01 14:14:35 +0400

    Fixes authenticate_user_please!

--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -1,6 +1,5 @@
 class Api::TopicsController < ApplicationController
-  skip_before_filter :verify_authenticity_token 
-  skip_before_filter :authenticate_user_please!
+  skip_before_filter :verify_authenticity_token
   
   respond_to :json
   
