9bf40c1a6e52583fd0ab760820fc43aa326e221a
Alexander Nusov <alex@local>
2013-09-08 10:00:24 +0000

    Updated shell script for sending push notifications

--- bin/apple-push-send.sh
+++ bin/apple-push-send.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 RAKE_BIN="/home/user/.rvm/bin/rake-ruby-2.0.0-p247"
+
 BASE_PATH="/home/user/knoda"
-APNS_PATH="/home/user/knoda/tools/apns-send/apns-send/bin/Release"
 APNS_SEND="mono /home/user/knoda/tools/apns-send/apns-send/bin/Release/apns-send.exe"
-MESSAGES_FILE=/tmp/apple.push.messages
-FAILED_FILE=/tmp/apple.push.failed
+APNS_MODE="production"
+
+CERT_FILE=/home/user/knoda/certs/APNs\ Production\ iOS.p12
+CERT_PASS=ENTER_PASSWORD_HERE
+
+
+if [ "$APNS_SANDBOX" = "yes" ]; then
+    CERT_FILE=/home/user/knoda/certs/APNs\ Development\ iOS.p12
+    CERT_PASS=111
+    APNS_MODE=sandbox
+else
+    APNS_SANDBOX="no"
+fi
+
+
+MESSAGES_FILE=/tmp/knoda.apple.push.messages
+FAILED_FILE=/tmp/knoda.apple.push.failed
+
+
+MESSAGES_FILE=$MESSAGES_FILE.$APNS_MODE
+FAILED_FILE=$FAILED_FILE.$APNS_MODE
 
 # Kill apns-send
 APNS_PID=`ps aux | grep apns-send | grep -v "grep" | head -n 1 | awk '{print $2}'`
@@ -18,7 +37,7 @@ fi
 echo > $MESSAGES_FILE
 
 # Export push messages
-(cd $BASE_PATH; $RAKE_BIN apns:export > $MESSAGES_FILE)
+(cd $BASE_PATH; $RAKE_BIN apns:export sandbox=$APNS_SANDBOX > $MESSAGES_FILE)
 
 if [[ $? != 0 ]]; then
    echo "Error: exporting push messages"
@@ -26,8 +45,7 @@ if [[ $? != 0 ]]; then
 fi
 
 # Send Push notifications
-cd $APNS_PATH
-$APNS_SEND $MESSAGES_FILE 1>$FAILED_FILE
+$APNS_SEND $APNS_MODE "$CERT_FILE" $CERT_PASS "$MESSAGES_FILE" 1>$FAILED_FILE
 
 # Fix wrong tokens
 (cd $BASE_PATH; $RAKE_BIN apns:process_failed file=$FAILED_FILE)
