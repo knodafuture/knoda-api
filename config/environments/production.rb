43c308f79e2b169bf624cb305db65f8945445638
Adam England <adam.n.england@gmail.com>
2013-11-07 22:27:21 -0600

    Updating prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Knoda::Application.configure do
   config.log_level = :debug
   config.eager_load = true
+  config.action_mailer.default_url_options = { :host => "localhost" }
+  config.reports_mailer_from = "example@example.com"
+  config.reports_mailer_to = "example@example.com"
+  config.user_mailer_from = "example@example.com"  
 end
