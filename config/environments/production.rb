9012841c5836664b9ca73b6aea1086e18d113878
Adam England <adam.n.england@gmail.com>
2013-11-10 15:21:43 -0600

    S3 tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,5 +20,7 @@ Knoda::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-  }    
+  }  
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'      
 end
