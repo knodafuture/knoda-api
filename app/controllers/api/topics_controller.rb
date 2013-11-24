6186e8f2949b34a206c627139fa9e3f3c65d3561
Adam England <adam.n.england@gmail.com>
2013-11-24 16:43:51 -0600

    Some caching for topics

--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -9,7 +9,10 @@ class Api::TopicsController < ApplicationController
     if params[:pattern]
       respond_with(Topic.find_active_by_pattern(params[:pattern]))
     else
-      respond_with(Topic.find_active)
+      @topics = Rails.cache.fetch("out_of_stock_products", :expires_in => 2.hours) do
+        Topic.find_active
+      end
+      respond_with(@topics)
     end
   end
 end
