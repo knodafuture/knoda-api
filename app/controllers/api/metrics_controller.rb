a413dd4d604e8df84257417ca47b84e6e63dd610
Adam England <adam.n.england@gmail.com>
2013-11-28 16:32:26 -0600

    Removing hardcoded lag

--- app/controllers/api/metrics_controller.rb
+++ app/controllers/api/metrics_controller.rb
@@ -4,7 +4,6 @@ class Api::MetricsController < ApplicationController
   def index    
     current = User.count
     lag7 = User.where("created_at < :start_date",{start_date: 7.days.ago}).count
-    lag7 = 44
     users = { :current => current, :lag7 => lag7}
     render :json => {:userMetrics => users, :status => "whatever"}
   end
