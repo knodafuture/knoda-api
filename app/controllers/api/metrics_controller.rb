9b14ec812af79dd17a35f26c68d3ad0e76dc0a85
Adam England <adam.n.england@gmail.com>
2013-12-04 13:15:33 -0600

    Add basic interaction metrics

--- app/controllers/api/metrics_controller.rb
+++ app/controllers/api/metrics_controller.rb
@@ -2,9 +2,8 @@ class Api::MetricsController < ApplicationController
   skip_before_filter :authenticate_user_please!
 
   def index    
-    current = User.count
-    lag7 = User.where("created_at < :start_date",{start_date: 7.days.ago}).count
-    users = { :current => current, :lag7 => lag7}
-    render :json => {:userMetrics => users, :status => "whatever"}
+    users = { :current => User.count, :lag7 => User.where("created_at < :start_date",{start_date: 7.days.ago}).count}
+    interaction = { :current => ((Comment.count + Challenge.count) /Prediction.count)}
+    render :json => {:userMetrics => users,  :interactionMetrics => interaction, :status => "whatever"}
   end
 end
