5082b62858fd4ce019b99d808a6a6ea0aaa39169
Adam England <adam.n.england@gmail.com>
2013-12-04 22:31:50 -0600

    More metrics

--- app/controllers/api/metrics_controller.rb
+++ app/controllers/api/metrics_controller.rb
@@ -4,7 +4,7 @@ class Api::MetricsController < ApplicationController
   def index    
     users = { :current => User.count, :lag7 => User.where("created_at < :start_date",{start_date: 7.days.ago}).count}
     commentRatio = { :current => ((Comment.count.to_f) / (Prediction.count.to_f))}
-    challengeRatio = { :current => ((Challenge.count.to_f) / (Prediction.count.to_f))}
+    challengeRatio = { :current => (((Challenge.count - Prediction.count).to_f) / (Prediction.count.to_f))}
     render :json => {:userMetrics => users,  :commentRatio => commentRatio, :challengeRatio => challengeRatio, :status => "whatever"}
   end
 end
