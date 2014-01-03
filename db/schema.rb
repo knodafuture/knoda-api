b322564c7476794e11b4530524d340e764601e30
Adam England <adam.n.england@gmail.com>
2014-01-03 11:31:28 -0600

    Add verified account flag for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131212030536) do
+ActiveRecord::Schema.define(version: 20140103172752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(version: 20131212030536) do
     t.datetime "avatar_updated_at"
     t.integer  "points",                 default: 0
     t.integer  "streak",                 default: 0
+    t.boolean  "verified_account",       default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
