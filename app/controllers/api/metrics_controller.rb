554291cab8c2c3430086d892d59aa043e094c030
Adam England <adam.n.england@gmail.com>
2014-02-19 21:25:28 -0600

    Fixing metrics

--- app/controllers/api/metrics_controller.rb
+++ app/controllers/api/metrics_controller.rb
@@ -5,7 +5,12 @@ class Api::MetricsController < ApplicationController
     users = { :current => User.count, :lag7 => User.where("created_at < :start_date",{start_date: 7.days.ago}).count}
     commentRatio = { :current => ((Comment.count.to_f) / (Prediction.count.to_f))}
     challengeRatio = { :current => (((Challenge.count - Prediction.count).to_f) / (Prediction.count.to_f))}
-    p = Prediction.tag_counts_on(:tags).order('tags_count DESC')
-    render :json => {:userMetrics => users,  :commentRatio => commentRatio, :challengeRatio => challengeRatio, :status => "whatever", :categories => p}
+    #p = Prediction.tag_counts_on(:tags).order('tags_count DESC')
+    p = Prediction.select("tags as name, count(*) as count").group("name")
+    categories = []
+    p.each do |tag|
+      categories << {:name => tag.name[0], :count => tag.count}
+    end
+    render :json => {:userMetrics => users,  :commentRatio => commentRatio, :challengeRatio => challengeRatio, :status => "whatever", :categories => categories}
   end
 end
