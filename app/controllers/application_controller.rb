8874644f875fb884fadad87a781475598e745370
Grant Isom <grantisom@ku.edu>
2014-05-21 16:08:12 -0500

    Changes made to sessions and application controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :authenticate_user_please!
   after_filter :clear_session
- 
+
   def clear_session
     session.clear
   end
@@ -15,26 +15,26 @@ class ApplicationController < ActionController::Base
   rescue_from ActionController::ParameterMissing do |exception|
     render json: {error: 'required parameter missing'}, status: 422
   end
- 
+
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: {error: 'item not found'}, status: 404
   end
-  
+
   protected
 
   def check_removed_api
     mv = Rails.application.config.minimum_version
     if derived_version < Rails.application.config.minimum_version
-      render json: {error: "Version not supported, mimimum version is #{mv}", status: :gone}
+      render json: {error: "Version not supported, mimimum version is #{mv}"}, status: :gone
     end
-  end  
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit :username, :email, :password
     end
   end
-  
+
   def authenticate_user_please!
     unless current_user
       respond_to do |format|
@@ -43,11 +43,11 @@ class ApplicationController < ActionController::Base
       end
     end
   end
-  
+
   def param_limit
     params[:limit] || 50
   end
-  
+
   def param_offset
     params[:offset] || 0
   end
@@ -59,11 +59,11 @@ class ApplicationController < ActionController::Base
   def param_id_gt
     params[:id_gt]
   end
-  
+
   def param_created_at_lt
     params[:created_at_lt]
   end
-  
+
   def pagination_meta(collection)
     if collection
       {offset: param_offset, limit: param_limit, count: collection.count}
