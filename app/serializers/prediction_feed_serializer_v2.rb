487f98db1adb44f5bbe1dcac2593f41bca0dd0b3
nick <nrellihan@gmail.com>
2014-05-29 15:43:51 -0500

    removing need for auth token for predictions_controller/index

--- app/serializers/prediction_feed_serializer_v2.rb
+++ app/serializers/prediction_feed_serializer_v2.rb
@@ -1,11 +1,16 @@
 class PredictionFeedSerializerV2 < PredictionSerializerV2
   self.root = false
-  
+
   attributes :my_challenge
   attributes :my_points
   attributes :resolution_date
-  
+
   def my_challenge
+
+    unless current_user
+      return nil
+    end
+
     l = object.challenges.select { |c| c.user_id == current_user.id}
     if l.length > 0
       return l[0]
@@ -13,7 +18,7 @@ class PredictionFeedSerializerV2 < PredictionSerializerV2
       return nil
     end
   end
-  
+
   def my_points
     if self.my_challenge
       self.my_challenge.points_details
