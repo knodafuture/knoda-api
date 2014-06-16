b9566604282c0af39501af98cebf77d827fe2789
Adam England <adam.n.england@gmail.com>
2014-06-16 15:13:52 -0500

    Allow conversion to named user for email signups

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -4,18 +4,29 @@ class Api::RegistrationsController < Devise::RegistrationsController
   skip_before_filter :require_no_authentication, only: [:create]
 
   def create
-    build_resource(sign_up_params)
-    if resource.save
-      UserEvent.new(:user_id => @user.id, :name => 'SIGNUP', :platform => get_signup_source()).save
-      set_flash_message :notice, :signed_up if is_navigational_format?
-      sign_up(resource_name, resource)
-      sign_in(resource_name, resource)
-      resource.reset_authentication_token!
-      resource.save!
-      return render :json => {:success => true, :auth_token => resource.authentication_token}
+    if current_user
+      resource = @current_user
+      resource.guest_mode = false
+      if resource.update(sign_up_params)
+        return render :json => {:success => true, :auth_token => current_user.authentication_token}
+      else
+        clean_up_passwords resource
+        return render :json => {:success => false, :errors => resource.errors}, :status => 400
+      end
     else
-      clean_up_passwords resource
-      return render :json => {:success => false, :errors => resource.errors}, :status => 400
+      build_resource(sign_up_params)
+      if resource.save
+        UserEvent.new(:user_id => @user.id, :name => 'SIGNUP', :platform => get_signup_source()).save
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        sign_in(resource_name, resource)
+        resource.reset_authentication_token!
+        resource.save!
+        return render :json => {:success => true, :auth_token => resource.authentication_token}
+      else
+        clean_up_passwords resource
+        return render :json => {:success => false, :errors => resource.errors}, :status => 400
+      end
     end
   end
 
