04580a8b8c7d34f5069f6017007ef250257c9c78
Adam England <adam.n.england@gmail.com>
2014-06-24 11:25:25 -0500

    Activity feed redesign
    Bumping core
    
    Bumping core
    
    Moving to develop core

---
+++ app/serializers/activity_serializer_v4.rb
@@ -0,0 +1,24 @@
+class ActivitySerializerV4 < ActiveModel::Serializer
+
+  attributes :id, :user_id, :activity_type, :target, :text, :created_at, :seen
+
+  self.root = false
+
+  def target
+    if object.activity_type == 'INVITATION'
+      return object.invitation_code
+    else
+      return object.prediction_id
+    end
+  end
+
+  def text
+    if object.activity_type == 'INVITATION'
+      return "<p><b>#{object.invitation_sender} has invited you to join the group</b> \"#{object.invitation_group_name}\"</p>"
+    elsif object.activity_type == 'COMMENT' and object.comment_body
+      return "<p><b>#{object.title}</b> \"#{object.comment_body}\"</p>"
+    else
+      return "<p><b>#{object.title}</b> \"#{object.prediction_body}\"</p>"
+    end
+  end
+end
