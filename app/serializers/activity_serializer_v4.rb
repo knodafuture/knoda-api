8b10f9878a9af390be1ccf8607eb400e7f6cf603
Adam England <adam.n.england@gmail.com>
2014-06-30 13:25:47 -0500

    Migrate old activities to new format
    Migrate data for new activities
    
    Update title in comment activities
    
    Safety checks
    
    Cleaning up won/lost text in serializer
    
    Cleaning up won/lost text in serializer
    
    Finishing activity migration

--- app/serializers/activity_serializer_v4.rb
+++ app/serializers/activity_serializer_v4.rb
@@ -1,6 +1,6 @@
 class ActivitySerializerV4 < ActiveModel::Serializer
 
-  attributes :id, :user_id, :activity_type, :target, :text, :title, :body, :created_at, :seen, :image_url
+  attributes :id, :user_id, :activity_type, :target, :title, :body, :created_at, :seen, :image_url
 
   self.root = false
 
@@ -12,35 +12,17 @@ class ActivitySerializerV4 < ActiveModel::Serializer
     end
   end
 
-  def text
-    if object.activity_type == 'INVITATION'
-      return "#{object.invitation_sender} has invited you to join the group \"#{object.invitation_group_name}\""
-    elsif object.activity_type == 'COMMENT' and object.comment_body
-      return "#{object.title} <span class='comment_body'>\"#{object.comment_body}\"</span>"
-    elsif object.activity_type == 'WON'
-      title = object.title
-      title.gsub!("You Won", "<span class='won_text'>You Won</span>")
-      return "#{title} \"#{object.prediction_body}\""
-    elsif object.activity_type == 'LOST'
-      title = object.title
-      title.gsub!("You Lost", "<span class='lost_text'>You Lost</span>")
-      return "#{object.title} \"#{object.prediction_body}\""
-    else
-      return "#{object.title} \"#{object.prediction_body}\""
-    end
-  end
-
   def title
     if object.activity_type == 'INVITATION'
       return "Hey-O! #{object.invitation_sender} has invited you to join a group"
     elsif object.activity_type == 'WON'
       title = object.title
-      title.gsub!("You Won", "<span class='won_text'>You Won</span>")
+      title.gsub!("You Won - ", "")
       return "#{title}"
     elsif object.activity_type == 'LOST'
       title = object.title
-      title.gsub!("You Lost", "<span class='lost_text'>You Lost</span>")
-      return "#{object.title}"
+      title.gsub!("You Lost - ", "")
+      return "#{title}"
     else
       return "#{object.title}"
     end
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,15 @@ module Knoda
     config.paths['db/migrate'] = KnodaCore::Engine.paths['db/migrate'].existent
     config.twitter_key = "14fSb3CT7EEQkoryO8RNx7BrG"
     config.twitter_secret = "6Z5OGzxLL9NqVEpAbLs9FFd2PyLm6pd7j5r98IZr5e0HRr73bo"
+    config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['S3_BUCKET_NAME'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
+    Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
   end
 end
