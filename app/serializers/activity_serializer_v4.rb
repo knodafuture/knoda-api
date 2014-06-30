615ebd9e04fbb5c8f95be61a479efd4a90b8a317
Adam England <adam.n.england@gmail.com>
2014-06-30 11:54:51 -0500

    Adding title & body for activity serializer

--- app/serializers/activity_serializer_v4.rb
+++ app/serializers/activity_serializer_v4.rb
@@ -1,6 +1,6 @@
 class ActivitySerializerV4 < ActiveModel::Serializer
 
-  attributes :id, :user_id, :activity_type, :target, :text, :created_at, :seen, :image_url
+  attributes :id, :user_id, :activity_type, :target, :text, :title, :body, :created_at, :seen, :image_url
 
   self.root = false
 
@@ -27,6 +27,30 @@ class ActivitySerializerV4 < ActiveModel::Serializer
       return "<p><b>#{object.title}</b> \"#{object.prediction_body}\"</p>"
     else
       return "<p><b>#{object.title}</b> \"#{object.prediction_body}\"</p>"
+    end
+  end
+
+  def title
+    if object.activity_type == 'INVITATION'
+      return "<p><b>#{object.invitation_sender} has invited you to join the group</b> \"#{object.invitation_group_name}\"</p>"
+    elsif object.activity_type == 'WON'
+      title = object.title
+      title.gsub!("You Won", "<span class='won_text'>You Won</span>")
+      return "<p><b>#{title}</b></p>"
+    elsif object.activity_type == 'LOST'
+      title = object.title
+      title.gsub!("You Lost", "<span class='lost_text'>You Lost</span>")
+      return "<p><b>#{object.title}</b></p>"
+    else
+      return "<p><b>#{object.title}</b></p>"
+    end
+  end
+
+  def body
+    if object.activity_type == 'COMMENT'
+      return object.comment_body
+    else
+      return object.prediction_body
     end
   end
 end
