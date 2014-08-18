b6511cdca945c2085eb94be46e3f04fdc9a2b1ff
Adam England <adam.n.england@gmail.com>
2014-08-17 21:03:00 -0500

    Add contact matching for facebook
    Bumping core

--- app/controllers/api/contact_matches_controller.rb
+++ app/controllers/api/contact_matches_controller.rb
@@ -3,16 +3,20 @@ class Api::ContactMatchesController < ApplicationController
   respond_to :json
 
   def create
-    contacts = params['_json']
-    User.uncached do
-      contacts.each do |i|
-        u = User.where('email in (?) OR phone in (?)', i[:emails], i[:phones]).first
-        if u
-          i[:knoda_info] = {:user_id => u.id, :username => u.username}
+    if params[:facebook]
+      render :json => current_user.facebook_friends_on_knoda, :root => false
+    else
+      contacts = params['_json']
+      User.uncached do
+        contacts.each do |i|
+          u = User.where('email in (?) OR phone in (?)', i[:emails], i[:phones]).first
+          if u
+            i[:knoda_info] = {:user_id => u.id, :username => u.username}
+          end
         end
       end
+      render :json => contacts, :root => false
     end
-    render :json => contacts, :root => false
   end
 
   private
