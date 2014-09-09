298d17e03d82f01a149e1880e91e8cd9462fc406
Adam England <adam.n.england@gmail.com>
2014-09-09 15:18:18 -0500

    Filter out self from contact matches

--- app/controllers/api/contact_matches_controller.rb
+++ app/controllers/api/contact_matches_controller.rb
@@ -17,7 +17,7 @@ class Api::ContactMatchesController < ApplicationController
             end
           end
           u = User.where('email in (?) OR phone in (?)', i[:emails], i[:phones]).first
-          if u
+          if u and u.id != current_user.id
             i[:knoda_info] = {:user_id => u.id, :username => u.username, :avatar_image => u.avatar_image, :following => current_user.led_by?(u)}
           end
         end
